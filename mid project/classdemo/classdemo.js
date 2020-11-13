4f4483422d9d6e6bda70a2c501937d8659bc0283
Iwamoto0228 <loayai0228@gmail.com>
2020-11-13 23:31:38 +0800

    1113 mid project updating

---
+++ mid project/classdemo/classdemo.js
@@ -0,0 +1,39 @@
+function change(index) {
+    const p = document.querySelector('#playarea');
+    console.log;
+    'p', p;
+    switch (index) {
+      case 1:
+        p.innerHTML =
+          '<iframe width=100% height=100% src="https://iwamoto0228.github.io/1091-1A-demo-407030039/w01/index.html" />';
+        break;
+      case 2:
+        p.innerHTML =
+          '<iframe width=100% height=100% src="https://iwamoto0228.github.io/1091-1A-demo-407030039/w02-imgGallery/imgGallery.html" />';
+        break;
+      case 3:
+        p.innerHTML =
+          '<iframe width=100% height=100% src="https://iwamoto0228.github.io/1091-1A-demo-407030039/w03-div/div.html" />';
+        break;
+      case 4:
+        p.innerHTML =
+          '<iframe width=100% height=100% src="https://iwamoto0228.github.io/1091-1A-demo-407030039/w03-div-tku60/div_tku60.html" />';
+        break;
+      case 5:
+        p.innerHTML =
+          '<iframe width=100% height=100% src="https://iwamoto0228.github.io/1091-1A-demo-407030039/w05%20nav/nav.html" />';
+        break;
+      case 6:
+        p.innerHTML =
+          '<iframe width=100% height=100% src="https://iwamoto0228.github.io/1091-1A-demo-407030039/w06/index.html" />';
+        break;
+      case 7:
+        p.innerHTML =
+          '<iframe width=100% height=100% src="https://iwamoto0228.github.io/1091-1A-demo-407030039/w07-ms-clone/index.html" />';
+        break;
+      case 8:
+        p.innerHTML =
+          '<iframe width=100% height=100% src="https://iwamoto0228.github.io/1091-1A-demo-407030039/w08/index.html" />';
+        break;  
+    }
+  }
